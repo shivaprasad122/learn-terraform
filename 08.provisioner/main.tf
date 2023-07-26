79ac24ac1565becfda0a4782d8072c14a3cf10fc
shiva <shivaprasad15mbt07@gmail.com>
2023-07-26 23:50:13 +0530

    terraform learn

---
+++ 08.provisioner/main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0a72e236e1fb571c5"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    inline = [
+      "sudo labauto ansible",
+       "ansible-pull -i localhost, -u something  "
+    ]
+  }
+}
+
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
