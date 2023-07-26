a90e43379d28f196954aefc6154316c602927ed4
shiva <shivaprasad15mbt07@gmail.com>
2023-07-26 16:26:26 +0530

    terraform

--- 03.vars/vars.tf
+++ 03.vars/vars.tf
@@ -17,3 +17,42 @@ output "sample1" {
 output "sample-ext" {
   value= "value of sample- $(var.sample)"
 }
+
+variable "course" {
+  default= "Devops training"
+}
+
+variable "courses" {
+  default = {
+    "Dveops",
+    "AWS",
+    "Pyhton"
+  }
+}
+
+variable "course_details" {
+  default= {
+    Devops = {
+      name= "Dveops"
+      timing= "10am"
+      duration=50
+    }
+    AWS= {
+      name = "AWS"
+      timing= "10am"
+      duration= 50
+    }
+  }
+}
+
+output "course" {
+  value=var.courses
+}
+
+output "courses" {
+  value=var.courses
+}
+
+output "course_details" {
+  value=var.course_details
+}
