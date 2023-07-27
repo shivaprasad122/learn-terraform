f38cae7570f8ba1a9849ef06bf06f444085aa42e
shiva <shivaprasad15mbt07@gmail.com>
2023-07-27 17:05:12 +0530

    terraform learn

---
+++ 10.loops/for-each/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "web" {
+  for_each = var.instances
+  ami           = data.aws_ami.example.id
+  instance_type = lookup(each.value, "instance_type", "t3.small")
+
+  tags = {
+    Name = each.key
+  }
+}
+
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+  default = {
+    frontend = {
+      name=frontend
+    }
+    catalogue = {
+      name=catalogue
+      instance_type = t3.nano
+    }
+    cart = {
+      name=cart
+      instance_type =t3.nano
+    }
+  }
+}
