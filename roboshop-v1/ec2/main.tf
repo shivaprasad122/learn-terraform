c6d6bacf246000e8f8f57523eb1bb490dd3f1b59
shiva <shivaprasad15mbt07@gmail.com>
2023-07-27 18:00:20 +0530

    terraform learn

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -1,22 +1,24 @@
-resource "aws_instance" "web" {
-  ami = data.aws_ami.example.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids= [aws_security_group.sg.id]
+ resource "aws_instance" "web" {
+   ami                    = data.aws_ami.example.id
+   instance_type          = "t3.micro"
+   vpc_security_group_ids = [aws_security_group.sg.id]
 
-  tags = {
-    Name = "var.name"
-  }
-
+   tags = {
+     Name = "var.name"
+   }
+ }
+  resource "null_resource" "ansible"
+     depends_on = [aws_instance.web,aws_route53_record.www]
   provisioner "remote-exec" {
 
   connection {
     type     = "ssh"
     user     = "centos"
     password = "DevOps321"
-    host     = self.public_ip
+    host     = aws_instance.web.public_ip
   }
     inline = [
-      "sudo labauto ansible",
+      "sudo lab-auto ansible",
       "ansible-pull -i localhost, -u something  "
     ]
   }
--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,55 +1,24 @@
-module "frontend" {
+module "instances" {
+  for_each =var.instances
   source ="./ec2"
-  name = "frontend"
+  name = "each.key"
 }
 
-module "mongodb" {
-  source = "./ec2"
-  name = "mongodb"
-}
+variable "instances" {
+    default = {
+      frontend = {}
+      mongodb = {}
+      catalogue = {}
+      mysql   = {}
+      user    = {}
+      redis   = {}
+      rabbitmq = {}
+      cart    = {}
+      shipping = {}
+      payment = {}
+      dispatch = {}
 
-module "catalogue" {
-  source ="./ec2"
-  name = "catalogue"
-}
 
-module "redis" {
-  source ="./ec2"
-  name = "redis"
-}
 
-module "user" {
-  source ="./ec2"
-  name = "user"
+  }
 }
-
-module "cart" {
-  source ="./ec2"
-  name = "cart"
-}
-
-module "mysql" {
-  source ="./ec2"
-  name = "mysql"
-}
-
-module "shipping" {
-  source ="./ec2"
-  name = "shipping"
-}
-
-module "rabbitmq" {
-  source ="./ec2"
-  name = "rabbitmq"
-}
-
-module "payment" {
-  source ="./ec2"
-  name = "payment"
-}
-
-module "dispatch" {
-  source ="./ec2"
-  name = "dispatch"
-}
-
