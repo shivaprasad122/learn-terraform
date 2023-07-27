ad64bd82a6210009b34b21f2fdb38c88dade8f70
shiva <shivaprasad15mbt07@gmail.com>
2023-07-27 11:14:43 +0530

    terraform learn

--- 11.functions/main.tf
+++ 11.functions/main.tf
@@ -5,3 +5,11 @@ variable "class" {
 output "class" {
   value =upper(var.class)
 }
+
+variable "fruits" {
+  default = ["apple","mango","banana"]
+}
+
+output "fruit_count" {
+  value=length(var.fruits)
+}
